bc99b3e24ecad0b465d49458a2f4a758dabf6ca4
void <voidnont@proton.me>
2026-09-19 03:07:35 +0200

    ios: add Vitr/VitrLinks.swift

---
+++ mobile/ios/Vitr/VitrLinks.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum VitrLinks {
+    static let player = URL(string: "https://vitr.nont.me/music")!
+    static let releasesAPI = URL(string: "https://api.github.com/repos/bloodvitr/vitr/releases?per_page=10")!
+    static let releasesPage = URL(string: "https://github.com/bloodvitr/vitr/releases")!
+    static let support = URL(string: "https://github.com/bloodvitr/vitr")!
+    static let donation = URL(string: "https://ko-fi.com/bloodvitr")!
+}
