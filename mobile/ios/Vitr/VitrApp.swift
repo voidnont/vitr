e2ef4ae5e877d89e631854576101cf85491c931b
void <voidnont@proton.me>
2026-09-19 03:07:32 +0200

    ios: add Vitr/VitrApp.swift

---
+++ mobile/ios/Vitr/VitrApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct VitrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
